110342674e90fba27a7abf0a23067ef2390ba13d
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-12-25 15:18:10 -0800

    Add fixed sidebar w/ Gmail, LinkedIn, GitHub icons & links.

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,15 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import logo from '/logo.svg';
 import './custom.scss';
 
 function Header() {
   const [scrollDirection, setScrollDirection] = useState("up");
 
+  // useEffect for handling navbar appearing/disappearing on scroll
   useEffect(() => {
+    // Track latest Y-direction of scroll
     let lastScrollY = window.scrollY;
 
+    // Set scrollDirection to "down" if scrolling down, to "up" if scrolling up
     const handleScroll = () => {
       if (window.scrollY > lastScrollY) {
         setScrollDirection("down");
@@ -17,20 +21,28 @@ function Header() {
         setScrollDirection("up");
       }
 
+      // Track latest Y-direction of scroll
       lastScrollY = window.scrollY;
     };
 
+    // Add event listener to call handleScroll upon scrolling
     window.addEventListener("scroll", handleScroll);
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // useEffect for hue that moves with mouse cursor
   React.useEffect(() => {
+    // Reference .scss element #hue-effect
     const hue = document.getElementById("hue-effect");
+
+    // Track X and Y-coordinate of mouse cursor
     const handleMouseMove = (e) => {
       hue.style.left = `${e.pageX}px`;
       hue.style.top = `${e.pageY}px`;
     };
+
+    // Add event listener to call handleMouseMove when moving mouse cursor
     window.addEventListener("mousemove", handleMouseMove);
 
     return () => window.removeEventListener("mousemove", handleMouseMove);
@@ -39,6 +51,18 @@ function Header() {
   return (
     <>
       <div id="hue-effect"></div>
+      <div className="fixed-left-sidebar d-flex flex-column align-items-center ms-4">
+        <a className="my-4 text-decoration-none" href="mailto:chjack568@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope className="fs-4" />
+        </a>
+        <a className="my-4 text-decoration-none" href="https://linkedin.com/in/jacky-cheng-54516a1ab" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="fs-4" />
+        </a>
+        <a className="my-4 text-decoration-none" href="https://github.com/jacheng1" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="fs-4" />
+        </a>
+        <div className="vertical-line"></div>
+      </div>
       <nav className={`navbar navbar-light bg-custom-blue p-4 fixed-top ${scrollDirection === "down" ? "hide" : "show"}`}>
         <div className="container-fluid">
           <img src={logo} className="logo" alt="Initials logo" width="50" height="50" />
