975d003194de81e0f8f447367d604626a4d9a950
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2025-01-05 23:06:20 -0800

    Modify backend index.js.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
+// Import necessary modules
 const express = require("express");
 const cors = require("cors");
 
+// Initialize Express application
 const app = express();
+
+// Define the port the server will listen on
 const PORT = process.env.PORT || 3000;
 
+// Setup Cors middleware
+app.use(cors());
+
+// GET route to check if backend is running correctly
 app.get("/", (req, res) => res.send("Backend is running."));
 
-app.listen(PORT, () => console.log(`Express server running on Port ${PORT}`));
-
-app.use(cors());
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Express server running on Port ${PORT}`);
+});
